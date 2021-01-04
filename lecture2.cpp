1099c081a2830cc2a2da67926f2320d65e5981b0
Op-panda <76877421+Op-panda@users.noreply.github.com>
2021-01-05 00:38:21 +0530

    Add files via upload

---
+++ lecture2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdlib.h>
+int main()
+{   system("cls");
+    double radius,area,perimeter;
+    cout<<"Enter radius of the circle:/n";
+    cin>>radius;
+    perimeter=2*3.14*radius;
+    area=3.14*radius*radius;
+    cout<<"the perimeter of the circle/n";
+    cout<<perimeter;
+    cout<<"the area of the circle/n";
+    cout<<area;
+    return 0;
+}
